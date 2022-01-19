2e9205a700cc958c864572acf73646568de4f2c6
Samanthap803b <samanthap803b@gmail.com>
2022-01-19 08:59:28 -0800

    almost done :( assessment 5

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,45 +15,81 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+// const secretCodeWord1 = "Lackadaisical"
+// // Expected output: "L4ck4d41s1c4l"
+// const secretCodeWord2 = "Gobbledygook"
+// // Expected output: "G0bbl3dyg00k"
+// const secretCodeWord3 = "Eccentric"
+// // Expected output: "3cc3ntr1c"
 
+// Psuedo code: Im going to start with describing my function what strCode .
+// After i write my "it" here Im goin to write what i want in return is my string to return in code.
+// after this i write my expect what i want in return expecifically. My output.
 
-
-// b) Create the function that makes the test pass.
-
-
-
+// describe("strCode", () =>{
+//   it("return a code message,The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.", () =>{
+//     const secretCodeWord1 = "Lackadaisical"
+//     const secretCodeWord2 = "Gobbledygook"
+//     const secretCodeWord3 = "Eccentric"
+//  expect(strCode(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+//  expect(strCode(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+//  expect(strCode(secretCodeWord3)).toEqual("3cc3ntr1c")
+//   })
+// })
+// // My test fail i got RED
+// // b) Create the function that makes the test pass.
+// // Now i have to create a function that is going to make my test pass.
+// // First i have to declare my variable strCode.
+// // After that i want to use conditionals my plan is to use .include to find the letters that i want to .replace
+// // In my if statement i want to say that if my string have "aAeEiIoO" in my new
+// const strCode = (string)=>{
+//   if (string.includes("aA") ||("eE")||("iI")||("oO")){
+//     var newStrCod = string.replace(/[aA]/g, '4').replace(/[eE]/g, '3').replace(/[iI]/g, '1').replace(/[oO]/g, '0')
+//   }
+//   return newStrCod
+// }
+//Its Green
+// IT tok me alont time to do this in my line 46 i was using let to declare my new variable but it wasent working I still dont know how is working with var.
+// The only reason i can think of is because my original value is const I check and that is not the reason im doing something wrong but I cant find it.
 // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
 const letterA = "a"
 // Expected output: ["Apple", "Banana", "Orange"]
-const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
-
-
+describe("justAorE", () =>{
+it("returns all the words that contain that particular letter a or e", () =>{
+const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+expect(justAorE(arrayOfWords1)).toEqual(["Apple", "Banana", "Orange"])
+expect(justAorE(arrayOfWords1)).toEqual(["Apple", "Banana", "Orange"])
+})
+ })
+// My test fail now im going to try to make it pass.
 // b) Create the function that makes the test pass.
+//First im going to declare my variable justAorE and i want to use .filter
+//with this i want .filter to go in my array and just return what i need.
+//i think im going to use .include to tell what i need.
 
+const justAorE = (array, string) => {
+  let newAr = array.filter ..........
+
+  
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
+// const hand1 = [5, 5, 5, 3, 3]
+// // Expected output: true
+// const hand2 = [5, 5, 3, 3, 4]
+// // Expected output: false
+// const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
 
--- interview-questions.md
+++ interview-questions.md
@@ -7,52 +7,51 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a block in Ruby?
 
-  Your answer:
+  Your answer: Blocks in Ruby is a method
 
-  Researched answer:
+  Researched answer: Ruby blocks are anonymous functions that can be passed into methods.Ruby has anonymous functions similar to JavaScript. They are called blocks. Blocks can be created with the do/end keywords OR with {}. Blocks are code that is passed to a method.
 
 
 
 2. What is a gem?
 
-  Your answer:
+  Your answer:Are packages with different type of functionalities.
 
-  Researched answer:
+  Researched answer:Gems can be used to extend or modify functionality in Ruby applications. A gem is library or snippet of code functionality.
 
 
 
 3. What is Ruby on Rails?
 
-  Your answer:
+  Your answer: Rains is a framework were we can build apps and we use ruby.
 
-  Researched answer:
+  Researched answer:Full stack framework, builds web app on front and back end.Ruby on Rails is a server-side web application framework written in Ruby.
 
 
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: ...I have to go back and read.
 
-  Researched answer:
+  Researched answer: columns and row structures almost like a Excel spreadsheet or Google sheets document with information displayed in rows and columns.
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
-
-  Researched answer:
+  Your answer:I think that is the number that at the beginning of my table and this works in case we have 2 things that are names the same, we can identify them by that number.
+  Researched answer:column that holds unique identifier usually number.They are important because this prevents problems like two Person instances being confused for each other because they have the same name. We can always tell them apart by their ID.
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: a rout that provides mapping from HTTP verbs.
 
-2. JSON:
+2. JSON: JavaScript object notation is a standard text-based format for representing structured data based on JavaScript object syntax.
 
-3. ERB:
+3. ERB: Embedded Ruby is a templating system that embeds ruby into a text document
 
 4. Params:
 
